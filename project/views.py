c9582352c54ebff0fa32df892163bcb330ce18b6
jessa <jessasayson29@gmail.com>
2014-05-13 17:49:34 +0800

    created index view

--- project/views.py
+++ project/views.py
@@ -10,3 +10,17 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
     return render_to_response('project/index.html', context_dict, context)
+
+def category(request, category_name_url):
+	context = RequestContext(request)
+	category_name = category_name_url.replace('_', ' ')
+	context_dict = {'category_name': category_name}
+
+	try:
+		category = Category.ogjects.get(name=category_name)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+    except Category.DoesNotExist:
+    	pass
+    return render_to_response('rango/category.html', context_dict, context)
